359aede7f5c3b175808d598d1922e5298a117dfd
phrugsa-limbunlom <phrugsa.lim@gmail.com>
2025-02-06 12:12:54 +0000

    add find product source node and revise search online query

--- chatbot-server/src/agent/SearchAgent.py
+++ chatbot-server/src/agent/SearchAgent.py
@@ -1,11 +1,12 @@
+import json
 import logging
 
 import requests
 from data.SearchAgentState import SearchAgentState
-
 from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate
 from langgraph.graph import StateGraph
+from playwright.sync_api import sync_playwright
 from service.VectorStoreService import VectorStoreService
 from text.PromptMessage import PromptMessage
 from text.WebURLs import WebURLs
@@ -25,6 +26,7 @@ class SearchAgent:
         graph.add_node("search_vector_store", self.search_vector_node)
         graph.add_node("search_online_shop", self.search_online_node)
         graph.add_node("analyze_and_rank", self.analyze_rank_node)
+        graph.add_node("find_product_source", self.find_source_node)
         graph.set_entry_point("analyze_query")
         graph.add_edge("analyze_query", "search_vector_store")
         graph.add_conditional_edges(
@@ -33,6 +35,8 @@ class SearchAgent:
             {False: "search_online_shop", True: "analyze_and_rank"}
         )
         graph.add_edge("search_online_shop", "analyze_and_rank")
+        # graph.add_edge("analyze_and_rank", "find_product_source")
+        graph.set_finish_point("analyze_and_rank")
         # self.graph = graph.compile(checkpointer=checkpointer)
         self.graph = graph.compile()
 
@@ -52,10 +56,8 @@ class SearchAgent:
             return response.choices[0].message.content
         except requests.exceptions.HTTPError as e:
             logger.error(f"HTTP Error occurred: {e.response.status_code} - {e.response.text}")
-            return "Sorry, I encountered an error while processing your request."
         except Exception as e:
             logger.error(f"An error occurred: {str(e)}")
-            return "Sorry, I encountered an unexpected error."
 
     def analyze_query_node(self, state: SearchAgentState):
 
@@ -98,7 +100,8 @@ class SearchAgent:
 
         for query in state['revised_query']:
 
-            response = self.tool.search(query=query, max_results=6)
+            query = f"find the specific product title from this product requirement: {query}"
+            response = self.tool.search(query=query, max_results=1)
 
             for r in response['results']:
                 products.append(r['content'])
@@ -115,10 +118,78 @@ class SearchAgent:
 
         return {"result": self.call_client(prompt=prompt)}
 
+    def find_source_node(self, state: SearchAgentState):
+
+        results = json.loads(state['result'])
+
+        for result in results["products"]:
+            for k, v in list(result.items()):
+                query = f"find source url for this product {v} from online shopping website"
+                response = self.tool.search(query=query, max_results=6)
+                result.update({"Source": response['results'][0]['url']})
+
+        return {"final_result": results}
+
+    def find_source_node(self, state: SearchAgentState):
+        with sync_playwright() as p:
+            results = json.loads(state['result'])
+
+            browser = p.chromium.launch(headless=False,
+                                        slow_mo=100,
+                                        args=[
+                                            '--disable-features=ImprovedCookieControls'
+                                        ])
+            context = browser.new_context(ignore_https_errors=True)
+
+            context.add_cookies([
+                {
+                    'name': 'session_id',
+                    'value': 'abc123',
+                    'domain': 'example.com',
+                    'path': '/'
+                }
+            ])
+            page = context.new_page()
+
+            for result in results["products"]:
+                for k, v in list(result.items()):
+
+                    page.goto(f"https://www.google.com")
+                    page.click('text=Accept')
+
+                    search_box = page.locator("input[name='q']")
+                    search_box.fill(v)
+                    search_box.press("Enter")
+
+                    page.wait_for_selector("text=Shopping")
+                    page.locator("text=Shopping").click()
+
+                    page.wait_for_selector(".sh-dgr__content", timeout=10000)
+
+                    product_cards = page.query_selector_all(".sh-dgr__content")
+
+                    for card in product_cards:
+                        try:
+                            link_element = card.query_selector("a")
+                            image_element = card.query_selector("img")
+
+                            r = {
+                                "link": link_element.get_attribute("href"),
+                                "image": image_element.get_attribute("src") if image_element else None
+                            }
+                            result.update(r)
+                            break
+                        except:
+                            continue
+
+            browser.close()
+
+        return {"final_result": results}
+
     def should_continue(self, state: SearchAgentState):
 
         if state["relevant_products"] == "":
-            print(f"Couldn't find the relevant products from vector store")
-            print(f"Start searching products online...")
+            logger.info(f"Couldn't find the relevant products from vector store")
+            logger.info(f"Start searching products online...")
 
         return state["relevant_products"] != ""
